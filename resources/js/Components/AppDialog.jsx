b7557677ad4f88fad2d7ad5bb135ab90c41a984a
Jose De avila <jrdeavila@unicesar.edu.co>
2023-09-25 08:48:25 -0700

    Bloquear Selecion texto del AppDialog

--- resources/js/Components/AppDialog.jsx
+++ resources/js/Components/AppDialog.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function AppDialog({ children, onClose, title }) {
     return (
         <div className="fixed inset-0 flex items-center justify-center  backdrop-blur-sm">
-            <div className="p-5 w-full mx-5  lg:w-1/2 lg:mx-0  bg-white  rounded-lg shadow-lg">
+            <div className="p-5 w-full mx-5  lg:w-1/2 lg:mx-0  bg-white  rounded-lg shadow-lg select-none">
                 <div className="flex items-center justify-between mb-5">
                     <h2 className="font-semibold text-2xl text-gray-800 ">
                         {title}
